6d365c4871dfc475d716599b1ca3484f9b5baa8d
Theunis Knoetze <theunis.knoetze@gmail.com>
2021-12-02 12:00:24 +0300

    cleaner code.

--- src/app.js
+++ src/app.js
@@ -15,40 +15,11 @@ function App() {
   const [hideRead, setHideRead] = useState(false)
   const [currentTab, setCurrentTab] = useState('inbox')
   const [searchTerm, setSearchterm] = useState("");
-  console.log(searchTerm);
 
   const unreadEmails = emails.filter(email => !email.read)
   const starredEmails = emails.filter(email => email.starred)
 
-  // const handleSearch = (e) => {
-  //   let searchEmails = emails;
-  //   let search = e.target.value;
-  //   let searchLength = e.target.value.length;
-  //   let inSearchEmails = [];
-  //   console.log(search)
-  //   searchEmails.map(email => {
-  //     for (let i = 0; i < email.title.length; i++) {
-  //       if (search === (email.title).toLowerCase().slice(i, searchLength) && searchLength > 0) {
-  //         email.inSearch = 1;
-  //         console.log(email);
-  //         inSearchEmails.push(email);
-  //       } if (searchLength === 0) {
-  //         inSearchEmails = initialEmails;
-  //       }
-  //     }
-  //   })
-  //   setEmails(inSearchEmails);
-  // }
-
-  const handleSearch = e => {
-    const searchString = e.target.value;
-    emails.filter(email => {
-      return email.title.contain()
-    })
-  }
-
   
-
   let filteredEmails = emails
 
   if (hideRead) filteredEmails = getReadEmails(filteredEmails)
